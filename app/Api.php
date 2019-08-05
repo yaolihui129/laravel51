8fe6dc82343b809831203d3c3779c463a12672ea
yaolihui129 <yaolihui129@sina.com>
2019-08-05 10:54:41 +0800

    周末的两个练习

--- app/Api.php
+++ app/Api.php
@@ -4,20 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Api extends Model
 {
     /*
      * 手动关联模型到指定的数据表
      * @var sting
      */
-    protected  $table = 'eo_user';
+    protected  $table = 'eo_api';
 //    指定主键
-    protected  $primaryKey = 'userID';
+    protected  $primaryKey = 'apiID';
 //    关闭时间戳
     protected  $timestamp = false;
 //    软删除
-//    protected $dates = ['deleted_at'];
-
-
-
+    protected $dates = ['removed'];
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('Test/sql', 'Test\SqlController' );
+Route::resource('Test/user', 'Test\UserController');
+Route::resource('Test/api', 'Test\ApiController');
